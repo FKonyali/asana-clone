fef1a5f5ead42cb24fde48286f3702d3db3de9ea
Furkan Konyali <furkan.konyali@kobil.com>
2022-01-22 21:19:47 +0300

    init project

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    Home
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Section from '@/components/Section.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Section
+  }
+}
+</script>
